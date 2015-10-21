df0f251beddf9ec28ac972f51a29c59f0cc685d2
aesoll <solladame@gmail.com>
2015-10-20 21:46:10 -0700

    Added test skeleton

---
+++ tests/test-astronomica-gen.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+#
+# test-astrometrica-gen.py
+#
+# Authors:
+#   Philipp v. Bieberstein (pbieberstein@email.arizona.edu)
+#   Matt Madrid (matthewmadrid@email.arizona.edu)
+#   David Sidi (dsidi@email.arizona.edu)
+#   Adam Soll (adamsoll@email.arizona.edu)
+"""
+
+"""
+
+
+import unittest
+
+
+class TestBuildDataframe(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+    	pass
+
+
+    def test_build_dataframe(self):
+    	pass
+
+
+class TestPassesMaster(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+    	pass
+
+
+    def test_passes_master(self):
+    	pass
+
+
+class TestSolveField(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+    	pass
+
+
+    def test_solve_field(self):
+    	pass
+
+
+class GenerateAstroConfig(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+    	pass
+
+
+    def test_generate_astro_config(self):
+    	pass
+
+
+if __name__=="__main__":
+	unittest.main()
