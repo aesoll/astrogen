f024cd71bebca23762a5802bdeb6bec1110474b1
Adam Soll <adam@mugatu-2.local>
2015-10-26 17:25:53 -0700

    Restructured directories and added necessary files

---
+++ bin/makeflow_gen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#
+# makeflow_gen.py
+#
+# Authors:
+#   Philipp v. Bieberstein (pbieberstein@email.arizona.edu)
+#   Matt Madrid (matthewmadrid@email.arizona.edu)
+#   David Sidi (dsidi@email.arizona.edu)
+#   Adam Soll (adamsoll@email.arizona.edu)
+#   Matthew Shanks (mcshanks@email.arizona.edu)
+"""
+
+"""
