bac75eb86f893041827a4a96bd033dcf1cfa9b5b
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2015-11-02 21:40:51 -0700

    fixed order of inputs

--- astrogen/makeflow_gen.py
+++ astrogen/makeflow_gen.py
@@ -59,8 +59,8 @@ def makeflow_gen(fits_filenames, fits_source_directory):
               '{}'.format(backend_config_path, filepath)
         file_name = '{}'.format(backend_config_path, filepath)
         makeflow_file.write(
-            "none"+ str(count) + " : " + "/gsfs1/xdisk/dsidi/midterm/astrometry.net\-0.50/blind/solve-field " + \
-            "/gsfs1/xdisk/dsidi/midterm/astrometriconf/resources/astrometry.cfg "+ filepath +"\n")
+            "none"+ str(count) + " : " + filepath + " /gsfs1/xdisk/dsidi/midterm/astrometry.net\-0.50/blind/solve-field " + \
+            +"\n")
         # TODO rm
         #     "/path/to/solve-field -u app -L 0.3 -H 3.0 --backend-config " +
         #     fits_source_directory + item + "\n"
